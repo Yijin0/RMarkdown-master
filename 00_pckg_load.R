64feff3dc81666fdcec0512da27ae7f88eb9a90d
Yijin0 <151764648+Yijin0@users.noreply.github.com>
2023-11-22 16:44:42 +0000

    Add files via upload

---
+++ 00_pckg_load.R
@@ -0,0 +1,11 @@
+install.packages("gapminder")
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("DT")
+install.packages("here")
+install.packages("knitr")
+install.packages("rticles")
+
+
